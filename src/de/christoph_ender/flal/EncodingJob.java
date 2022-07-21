e1c09dfd5219828b8ad35e8220080e4f06070259
Christoph Ender <mail@christoph-ender.de>
2022-07-21 19:12:31 +0200

    Removed unused imports.

--- src/de/christoph_ender/flal/EncodingJob.java
+++ src/de/christoph_ender/flal/EncodingJob.java
@@ -2,12 +2,7 @@
 package de.christoph_ender.flal;
 
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
